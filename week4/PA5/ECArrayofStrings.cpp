de5079f51249e48fe7443bf3408daf76ebd8fb4d
unknown <larbear1337@gmail.com>
2020-02-10 09:37:16 -0500

    PA5 added

---
+++ week4/PA5/ECArrayofStrings.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "ECArrayofStrings.h" 
+
+using namespace std;
+
+ECArrayofStrings :: ECArrayofStrings()
+{
+}
+
+ECArrayofStrings :: ~ECArrayofStrings()
+{
+  // your code goes here
+}
+
+void ECArrayofStrings :: AddString( const string &strToAdd )
+{
+  // your code goes here
+}
+
+int ECArrayofStrings :: GetNumofStrings() const
+{
+  // your code goes here
+}
+
+int ECArrayofStrings :: GetMaxLen() const
+{
+  // your code goes here
+}
+
+string ECArrayofStrings :: GetLongestCommonPrefix() const
+{
+  // your code goes here
+}
+
+string ECArrayofStrings :: Concatnate() const
+{
+  // your code goes here
+}
+
+void ECArrayofStrings:: Dump() const
+{
+  // your code goes here
+}
+
+string ECArrayofStrings :: GetLongestCommonPrefixofTwo(const string &str1, const string &str2) const
+{
+  // your code goes here
+}
+
+
